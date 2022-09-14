ea707b74eddea961eb8a33210f84652cbdbf9a0c
Jonatas Queiroz Lima <joqlima5@gmail.com>
2022-09-14 18:32:13 -0300

    add: export  in vehicleSchema

--- src/interfaces/IVehicle.ts
+++ src/interfaces/IVehicle.ts
@@ -1,6 +1,6 @@
 import { z } from 'zod';
 
-const vehicleSchema = z.object({
+export const vehicleSchema = z.object({
   model: z.string().min(3),
   year: z.number().min(1900).max(2022),
   color: z.string().min(3),
