f5a0ca889712b7ef2754f5a843bee8166aa3e780
Jonatas Queiroz Lima <joqlima5@gmail.com>
2022-09-14 19:54:19 -0300

    fix: missing correct schema structure

--- src/models/Car.model.ts
+++ src/models/Car.model.ts
@@ -1,7 +1,16 @@
 import { model, Schema } from 'mongoose';
 import { ICar } from '../interfaces/ICar';
 
-const carSchema = new Schema<ICar>();
+const carSchema = new Schema<ICar>({
+  model: String,
+  color: String,
+  buyValue: Number,
+  doorsQty: Number,
+  seatsQty: Number,
+  status: Boolean,
+  year: Number,
+}, { versionKey: false });
+
 const CarModel = model('Car', carSchema);
 
 export default CarModel;
