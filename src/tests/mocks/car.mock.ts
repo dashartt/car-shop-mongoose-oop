3fa51d6e79d9feadc24433a3ccebb3b7a6e8b079
Jonatas Queiroz Lima <joqlima5@gmail.com>
2022-09-14 18:31:49 -0300

    add: new data mock

--- src/tests/mocks/car.mock.ts
+++ src/tests/mocks/car.mock.ts
@@ -1,28 +1,37 @@
 import { ICar } from "../../interfaces/ICar";
 
-const carMock : ICar = {
-    color: 'Preto',
-    doorsQty: 4, 
-    seatsQty: 4,
-    model: 'Classic',
-    year: 2001,
-    status: false,
-    buyValue: 200  
-  };
+export const invalidCarMock: ICar = {
+  color: 'Preto',
+  doorsQty: 11,
+  seatsQty: -1,
+  model: 'Classic',
+  year: 2001,
+  status: false,
+  buyValue: 200
+};
 
-  export const carrMockWithIdUpdated : ICar = {
-    color: 'Preto',
-    doorsQty: 4, 
-    seatsQty: 4,
-    model: 'Classic',
-    year: 2001,
-    status: true,
-    buyValue: 200  
-  };
-  
-  const carMockWithId: ICar & { _id : string } = {
-    _id: '62cf1fc6498565d94eba52cd',      
-    ...carMock
-  };  
-  
-  export { carMock, carMockWithId };
+export const carMock: ICar = {
+  color: 'Preto',
+  doorsQty: 4,
+  seatsQty: 4,
+  model: 'Classic',
+  year: 2001,
+  status: false,
+  buyValue: 200
+};
+
+export const carrMockWithIdUpdated: ICar = {
+  color: 'Preto',
+  doorsQty: 4,
+  seatsQty: 4,
+  model: 'Classic',
+  year: 2001,
+  status: true,
+  buyValue: 200
+};
+
+export const carMockWithId: ICar & { _id: string } = {
+  _id: '62cf1fc6498565d94eba52cd',
+  ...carMock
+};
+
