8f70a6c73f280fa800d42d12997996ec40d3aa59
Jonatas Queiroz Lima <joqlima5@gmail.com>
2022-09-14 17:49:27 -0300

    edit: export carSchema too

--- src/interfaces/ICar.ts
+++ src/interfaces/ICar.ts
@@ -1,7 +1,7 @@
 import { z } from 'zod';
 import { IVehicle } from './IVehicle';
 
-const carSchema = z.object({
+export const carSchema = z.object({
   doorsQty: z.number().min(2).max(4),
   seatsQty: z.number().min(2).max(7),
 });
