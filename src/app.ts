8d06738a75c30a513881f3c4b5ca8e4a6b599e93
Jonatas Queiroz Lima <joqlima5@gmail.com>
2022-09-14 19:15:19 -0300

    add: import express async errors

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,12 @@
 import express from 'express';
+import 'express-async-errors';
+import errorHandler from './middlewares/error';
+import carRoutes from './routes/cars.route';
 
 const app = express();
+
+app.use(express.json());
+app.use('/cars', carRoutes);
+app.use(errorHandler);
 
 export default app;
