f8d8813f2b6e29317b4cc9a27928a6c528dbfd9d
Jonatas Queiroz Lima <joqlima5@gmail.com>
2022-09-18 15:13:43 -0300

    add: new car mocks

--- src/tests/mocks/car.mock.ts
+++ src/tests/mocks/car.mock.ts
@@ -1,6 +1,6 @@
 import { ICar } from "../../interfaces/ICar";
 
-export const invalidCarMock: ICar = {
+export const invalidCar: ICar = {
   color: 'Preto',
   doorsQty: 11,
   seatsQty: -1,
@@ -10,7 +10,7 @@ export const invalidCarMock: ICar = {
   buyValue: 200
 };
 
-export const carMock: ICar = {
+export const car: ICar = {
   color: 'Preto',
   doorsQty: 4,
   seatsQty: 4,
@@ -20,18 +20,54 @@ export const carMock: ICar = {
   buyValue: 200
 };
 
-export const carrMockWithIdUpdated: ICar = {
-  color: 'Preto',
-  doorsQty: 4,
-  seatsQty: 4,
-  model: 'Classic',
-  year: 2001,
+export const updateCar: ICar = {
+  color: 'updated_color',
+  doorsQty: 2,
+  seatsQty: 2,
+  model: 'update_model',
+  year: 2007,
   status: true,
-  buyValue: 200
+  buyValue: 777
 };
 
-export const carMockWithId: ICar & { _id: string } = {
+export const updatedCarWithId: ICar & { _id: string }= {
   _id: '62cf1fc6498565d94eba52cd',
-  ...carMock
+  ...updateCar
 };
+
+
+export const carWithId: ICar & { _id: string } = {
+  _id: '62cf1fc6498565d94eba52cd',
+  ...car
+};
+
+export const carskWithId: (ICar & { _id: string })[]  = [
+  { ...carWithId },
+  { 
+    _id: '62cf1fd6498565d94eba52cd',
+    color: 'color_#',
+    doorsQty: 2,
+    seatsQty: 2,
+    model: 'model_2xx1',
+    year: 2001,
+    status: false,
+    buyValue: 111,
+  },
+  {
+    _id: '62cf1fc6498565d94eba53cd',
+    color: 'color_#2',
+    doorsQty: 3,
+    seatsQty: 3,
+    model: 'model_2xx2',
+    year: 2002,
+    status: true,
+    buyValue: 222
+  }
+]
+
+export const carsAfterDeleteOne = carskWithId.shift();
+
+export const invalidId = '62cf1fc6498565d94eba52c';
+export const nonExistId = '62cf1fc6498565d94eba52cc';
+
 
